7bf70f89832c6f3c5b1ccacb8c90162cafcb896d
Avinendra <avinendra08@gmail.com>
2025-12-03 10:16:51 +0530

    user crud and password encryption

--- app/crud.py
+++ app/crud.py
@@ -4,6 +4,7 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 from sqlmodel import Session
+from .utils import hash
 from . import models , schemas
 from .database import  engine , get_db
 
@@ -54,6 +55,8 @@ app = FastAPI()
 def root():
     return {"message":"Hey!!"}
 
+
+#######   posts operations
 
 ##get all posts
 @app.get("/posts" ,status_code = status.HTTP_200_OK , response_model= List[schemas.PostResponse])
@@ -130,3 +133,28 @@ def update_post(id: int, post: schemas.PostCreate , db: Session = Depends(get_db
     db.commit()
 
     return post_query.first()
+
+
+
+##users operations
+
+##create a user
+@app.post("/users",status_code = status.HTTP_201_CREATED , response_model=schemas.UserOut)
+def createUser(user : schemas.UserCreate , db: Session = Depends(get_db)):
+    hashed_password = hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)  # get newly created row with ID
+    return new_user 
+
+
+##get user by id
+@app.get("/users/{id}" ,status_code = status.HTTP_200_OK ,response_model=schemas.UserOut)
+def get_posts(id:int , db : Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail=f"user with id : {id} does not exist")
+    
+    return user
